d898f7a16a91fecec89b3e0e8fc217a077fe4cd9
songlairui <cnre@live.cn>
2019-01-12 00:33:31 +0800

    改为decorator模式

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,33 @@
 <template>
   <div id="app">
     <control :debugging="true" :touching.sync="touching" :direction.sync="direction">
-      <pre>{{ info }}</pre>
+      <pre class="info">{{ info }}</pre>
     </control>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import { Vue, Component } from 'vue-property-decorator';
 
-export default Vue.extend({
-  name: 'app',
-  data() {
-    return {
-      direction: 1,
-      touching: false,
-    };
-  },
-  computed: {
-    info(): string {
-      const touching = this.touching ? 'touching' : '--------';
-      return (this.direction === -1
-        ? ['←', touching, '  ']
-        : ['  ', touching, '→']
-      ).join(' ');
-    },
-  },
-});
+@Component
+export default class App extends Vue {
+  direction: number = 1;
+  touching: boolean = false;
+  get info(): string {
+    const touching = this.touching ? 'touching' : '--------';
+    return (this.direction === -1
+      ? ['←', touching, '  ']
+      : ['  ', touching, '→']
+    ).join(' ');
+  }
+}
 </script>
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Courier New', Courier, monospace;
+}
+.info {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
